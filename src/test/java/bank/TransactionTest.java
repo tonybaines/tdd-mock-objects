b6663bc0c0d6f3fb666f1553b79bef7b2eec2e09
Tony Baines <tony.baines@bt.com>
2012-12-04 17:06:08 +0000

    Added the story to work through

---
+++ src/test/java/bank/TransactionTest.java
@@ -0,0 +1,13 @@
+package bank;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+import org.junit.Test;
+
+public class TransactionTest {
+        @Test
+        public void startHere() {
+                fail("No test yet");
+        }
+}
+
